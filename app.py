74f76989fafb7d33bf14322006ff489404bb79e1
NikkoPacleb <pacleb.nikko@gmail.com>
2023-10-03 23:16:24 -0700

    Sample Iteration of Transactions

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
+import datetime 
 from flask import Flask, render_template
 app = Flask(__name__)
 
+transactions = [
+    {
+        'TransactionID': '1',
+        'TransactionDate': datetime.datetime(2020, 1, 1),
+        'TransactionAmount': 1000,
+    },
+    {
+        'TransactionID': '2',
+        'TransactionDate': datetime.datetime(2020, 1, 2),
+        'TransactionAmount': 2000,
+    },
+    {
+        'TransactionID': '3',
+        'TransactionDate': datetime.datetime(2020, 1, 1),
+        'TransactionAmount': 3000,
+    }
+
+]
+
 @app.route('/')
 def hello_geek():
-    return render_template('index.html')
+    return render_template('index.html', 
+                           transactions=transactions,
+                           title='Home')
 
 
 if __name__ == "__main__":
