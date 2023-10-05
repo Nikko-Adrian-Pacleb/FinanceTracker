e9b1a5018486485ee8229d6200fa1be5bcf5057b
NikkoPacleb <pacleb.nikko@gmail.com>
2023-10-04 20:34:23 -0700

    Commit from jaspy-c conflict fixed

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
+import datetime 
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/', methods=["GET","POST"])
-def hello_geek():
-    return render_template('dashboard.html')
+transactions = [
+    {
+        'TransactionID': '1',
+        'TransactionDate': datetime.datetime(2020, 1, 1),
+        'TransactionAmount': 1000,
+    },
+    {
+        'TransactionID': '2',
+        'TransactionDate': datetime.datetime(2020, 1, 2),
+        'TransactionAmount': 2000,
+    },
+    {
+        'TransactionID': '3',
+        'TransactionDate': datetime.datetime(2020, 1, 1),
+        'TransactionAmount': 3000,
+    }
 
+]
+
+@app.route('/', methods=["GET","POST"])
+def get_home():
+    return render_template('dashboard.html',
+                           transactions=transactions
+                            )
 
 if __name__ == "__main__":
     app.run(debug=True)
